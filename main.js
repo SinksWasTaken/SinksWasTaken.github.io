0e77dc4651ed27320710e050fa11aaba59cc65ac
SinksWasTaken <103905629+SinksWasTaken@users.noreply.github.com>
2024-06-21 21:35:27 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,70 @@
-let names = ["George", "Jimmy", "Bob"];
+let names = [
+    "Aaron", "Abigail", "Adam", "Adrian", "Aidan", "Aiden", "Alan", "Albert", "Alejandro", "Alex", 
+    "Alexander", "Alexandra", "Alice", "Alicia", "Allison", "Alyssa", "Amanda", "Amber", "Amy", "Andrea", 
+    "Andrew", "Angela", "Angelica", "Anna", "Anthony", "Antonio", "April", "Ariana", "Arianna", "Ashley", 
+    "Austin", "Ava", "Barbara", "Benjamin", "Bethany", "Blake", "Brandon", "Brayden", "Breanna", "Brenda", 
+    "Brian", "Brianna", "Brittany", "Brody", "Brooklyn", "Bryan", "Bryce", "Caleb", "Cameron", "Carlos", 
+    "Caroline", "Carter", "Cassandra", "Catherine", "Cecilia", "Chad", "Charles", "Charlotte", "Chase", 
+    "Chelsea", "Chloe", "Christian", "Christina", "Christopher", "Claire", "Clara", "Clarence", "Clayton", 
+    "Cody", "Colby", "Cole", "Colin", "Collin", "Connor", "Cooper", "Courtney", "Crystal", "Curtis", 
+    "Dakota", "Dale", "Damian", "Daniel", "Danielle", "David", "Dean", "Deborah", "Delaney", "Derek", 
+    "Destiny", "Devin", "Diana", "Dominic", "Donald", "Donna", "Dylan", "Edward", "Edwin", "Elena", 
+    "Eli", "Eliana", "Elias", "Elijah", "Elizabeth", "Ella", "Elliot", "Emily", "Emma", "Eric", 
+    "Erica", "Erik", "Ethan", "Eva", "Evan", "Evelyn", "Faith", "Fernando", "Fiona", "Francesca", 
+    "Francisco", "Frank", "Gabriel", "Gabriella", "Gabrielle", "Gage", "Garrett", "Gavin", "Genesis", "George", 
+    "Georgia", "Gianna", "Giselle", "Grace", "Grant", "Gregory", "Hailey", "Haley", "Hannah", "Hayden", 
+    "Heather", "Heidi", "Helen", "Henry", "Holly", "Hope", "Hudson", "Hunter", "Ian", "Isaac", 
+    "Isabel", "Isabella", "Isabelle", "Isaiah", "Ivan", "Jack", "Jackson", "Jacob", "Jaden", "Jake", 
+    "James", "Jasmine", "Jason", "Javier", "Jayden", "Jeffrey", "Jenna", "Jennifer", "Jeremy", "Jessica", 
+    "Jesus", "Jillian", "Joanna", "Jocelyn", "Joel", "John", "Johnny", "Jonathan", "Jordan", "Jorge", 
+    "Jose", "Joseph", "Joshua", "Josiah", "Joy", "Juan", "Julia", "Julian", "Julie", "Justin", 
+    "Kaitlyn", "Katherine", "Kathryn", "Katie", "Kayla", "Kaylee", "Keith", "Kelly", "Kelsey", "Kendall", 
+    "Kenneth", "Kevin", "Kiara", "Kimberly", "Kyle", "Kylie", "Landon", "Laura", "Lauren", "Leah", 
+    "Leonardo", "Liam", "Lillian", "Lily", "Logan", "Lucas", "Luis", "Luke", "Mackenzie", "Madeline", 
+    "Madelyn", "Madison", "Makayla", "Maria", "Mariah", "Marissa", "Mark", "Mason", "Matthew", "Megan", 
+    "Melanie", "Melissa", "Michael", "Michelle", "Miguel", "Mikayla", "Miles", "Molly", "Morgan", "Naomi", 
+    "Natalie", "Nathan", "Nathaniel", "Nevaeh", "Nicholas", "Nicole", "Noah", "Nolan", "Olivia", "Owen", 
+    "Paige", "Parker", "Patrick", "Paul", "Payton", "Penelope", "Peter", "Peyton", "Philip", "Piper", 
+    "Quinn", "Rachel", "Raegan", "Raymond", "Rebecca", "Richard", "Riley", "Robert", "Rocco", "Ruby", 
+    "Ryan", "Sabrina", "Sadie", "Samantha", "Samuel", "Sara", "Sarah", "Savannah", "Scott", "Sean", 
+    "Sebastian", "Serenity", "Seth", "Shane", "Shawn", "Shelby", "Sierra", "Sophia", "Sophie", "Spencer", 
+    "Stella", "Stephanie", "Steven", "Summer", "Sydney", "Tanner", "Taylor", "Thomas", "Timothy", "Travis", 
+    "Trevor", "Trinity", "Tristan", "Tyler", "Valeria", "Valerie", "Vanessa", "Veronica", "Victoria", "Vincent", 
+    "Violet", "Vivian", "William", "Wyatt", "Xavier", "Zachary", "Zoe", "Zoey", "Aaron", "Abigail", 
+    "Adam", "Adrian", "Aidan", "Aiden", "Alan", "Albert", "Alejandro", "Alex", "Alexander", "Alexandra", 
+    "Alice", "Alicia", "Allison", "Alyssa", "Amanda", "Amber", "Amy", "Andrea", "Andrew", "Angela", 
+    "Angelica", "Anna", "Anthony", "Antonio", "April", "Ariana", "Arianna", "Ashley", "Austin", "Ava", 
+    "Barbara", "Benjamin", "Bethany", "Blake", "Brandon", "Brayden", "Breanna", "Brenda", "Brian", "Brianna", 
+    "Brittany", "Brody", "Brooklyn", "Bryan", "Bryce", "Caleb", "Cameron", "Carlos", "Caroline", "Carter", 
+    "Cassandra", "Catherine", "Cecilia", "Chad", "Charles", "Charlotte", "Chase", "Chelsea", "Chloe", "Christian", 
+    "Christina", "Christopher", "Claire", "Clara", "Clarence", "Clayton", "Cody", "Colby", "Cole", "Colin", 
+    "Collin", "Connor", "Cooper", "Courtney", "Crystal", "Curtis", "Dakota", "Dale", "Damian", "Daniel", 
+    "Danielle", "David", "Dean", "Deborah", "Delaney", "Derek", "Destiny", "Devin", "Diana", "Dominic", 
+    "Donald", "Donna", "Dylan", "Edward", "Edwin", "Elena", "Eli", "Eliana", "Elias", "Elijah", 
+    "Elizabeth", "Ella", "Elliot", "Emily", "Emma", "Eric", "Erica", "Erik", "Ethan", "Eva", 
+    "Evan", "Evelyn", "Faith", "Fernando", "Fiona", "Francesca", "Francisco", "Frank", "Gabriel", "Gabriella", 
+    "Gabrielle", "Gage", "Garrett", "Gavin", "Genesis", "George", "Georgia", "Gianna", "Giselle", "Grace", 
+    "Grant", "Gregory", "Hailey", "Haley", "Hannah", "Hayden", "Heather", "Heidi", "Helen", "Henry", 
+    "Holly", "Hope", "Hudson", "Hunter", "Ian", "Isaac", "Isabel", "Isabella", "Isabelle", "Isaiah", 
+    "Ivan", "Jack", "Jackson", "Jacob", "Jaden", "Jake", "James", "Jasmine", "Jason", "Javier", 
+    "Jayden", "Jeffrey", "Jenna", "Jennifer", "Jeremy", "Jessica", "Jesus", "Jillian", "Joanna", "Jocelyn", 
+    "Joel", "John", "Johnny", "Jonathan", "Jordan", "Jorge", "Jose", "Joseph", "Joshua", "Josiah", 
+    "Joy", "Juan", "Julia", "Julian", "Julie", "Justin", "Kaitlyn", "Katherine", "Kathryn", "Katie", 
+    "Kayla", "Kaylee", "Keith", "Kelly", "Kelsey", "Kendall", "Kenneth", "Kevin", "Kiara", "Kimberly", 
+    "Kyle", "Kylie", "Landon", "Laura", "Lauren", "Leah", "Leonardo", "Liam", "Lillian", "Lily", 
+    "Logan", "Lucas", "Luis", "Luke", "Mackenzie", "Madeline", "Madelyn", "Madison", "Makayla", "Maria", 
+    "Mariah", "Marissa", "Mark", "Mason", "Matthew", "Megan", "Melanie", "Melissa", "Michael", "Michelle", 
+    "Miguel", "Mikayla", "Miles", "Molly", "Morgan", "Naomi", "Natalie", "Nathan", "Nathaniel", "Nevaeh", 
+    "Nicholas", "Nicole", "Noah", "Nolan", "Olivia", "Owen", "Paige", "Parker", "Patrick", "Paul", 
+    "Payton", "Penelope", "Peter", "Peyton", "Philip", "Piper", "Quinn", "Rachel", "Raegan", "Raymond", 
+    "Rebecca", "Richard", "Riley", "Robert", "Rocco", "Ruby", "Ryan", "Sabrina", "Sadie", "Samantha", 
+    "Samuel", "Sara", "Sarah", "Savannah", "Scott", "Sean", "Sebastian", "Serenity", "Seth", "Shane", 
+    "Shawn", "Shelby", "Sierra", "Sophia", "Sophie", "Spencer", "Stella", "Stephanie", "Steven", "Summer", 
+    "Sydney", "Tanner", "Taylor", "Thomas", "Timothy", "Travis", "Trevor", "Trinity", "Tristan", "Tyler", 
+    "Valeria", "Valerie", "Vanessa", "Veronica", "Victoria", "Vincent", "Violet", "Vivian", "William", "Wyatt", 
+    "Xavier", "Zachary", "Zoe", "Zoey"
+]
+
 let imgs = 3;
 let ctr = 1;
 
@@ -6,10 +72,19 @@ let btns = document.getElementById('theButtons');
 
 function createImg()
 {
-    let a = "url(Cats/"
-    a += String(ctr);
-    a += ".png)";
-
+    if(ctr>400)
+        {
+            let a = "url(Cats/t"
+            a += String(ctr-400);
+            a += ".png)";
+        }
+    else
+        {
+            let a = "url(Cats/"
+            a += String(ctr);
+            a += ".png)";
+        }
+    
     return a;
 }
 
